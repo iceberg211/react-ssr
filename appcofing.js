f14da4f877aeeb9f290de243ee9747c2302e3251
何炜 <hewei@77ircloud.com>
2018-06-21 20:32:50 +0800

    增加生产配置文件

---
+++ appcofing.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cdn: {
+    ak: '9Hzs5SXgPP5UMRzStRrJF9penkMA5RZq2NBQZREh',
+    sk: 'cC8vM0IJDytn__RsapOdlF0Tyki2xbvvM6Jk_8kj',
+    bucket: 'react-ssr',
+    host: 'http://p9uf55l28.bkt.clouddn.com'
+  }
+}
--- webpack/webpack.config.client.js
+++ webpack/webpack.config.client.js
@@ -53,5 +53,36 @@ if (isDev) {
     app: ['react-hot-loader/patch', path.join(__dirname, '../client/app.js')]
 
   }
+} else {
+  config.entry = {
+    app: path.join(__dirname, '../client/app.js'),
+    vendor: ['react', 'react-dom', 'react-router-dom', 'mobx', 'mobx-react', 'axios']
+  }
+  config.output.filename = '[name].[chunkhash].js'
+  config.plugins.push(
+    new Webpack.optimize.UglifyJsPlugin(),
+    new Webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new Webpack.optimize.CommonsChunkPlugin({
+      name: 'mainfest',
+      minChunks: Infinity
+    }),
+    // 命名异步模块
+    new Webpack.NamedModulesPlugin(),
+    new Webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    // 给每一个模块命名，防止已数字来命名
+    new Webpack.NamedChunksPlugin((chunk) => {
+      if (chunk.name) {
+        return chunk.name
+      }
+      return chunk.mapModules(m => path.relative(m.context, m.request)).join('_')
+    })
+  )
 }
+
 module.exports = config
