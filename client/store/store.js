c6c64edc07fabe83bc3bc3e6aa6a6fe0ecf54340
hewei <187399692@qq.com>
2018-06-23 13:52:59 +0800

    saga和devTools的解决

--- client/store/store.js
+++ client/store/store.js
@@ -3,8 +3,9 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-  const debug = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__()  // eslint-disable-line
-    : f => f;
+  // const debug = window.__REDUX_DEVTOOLS_EXTENSION__ ?
+  // window.__REDUX_DEVTOOLS_EXTENSION__()  // eslint-disable-line
+  //   : f => f;
 
   if (initialState) {
     return createStore(
